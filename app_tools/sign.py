8dbdf363804e3c3e969e14483a58738ba2caa004
Atidipt123 <atidiptmishra@gmail.com>
2022-01-14 12:49:22 +0530

    add login and register

---
+++ app_tools/sign.py
@@ -0,0 +1,50 @@
+import json
+import base64
+import os
+
+def register() -> int:
+    name = input("ENTER YOUR NAME: ")
+
+    while True:
+        pwd = input("ENTER A PASSWORD: ")
+        if len(pwd) < 8:
+            print("Password must be 8 characters long")
+        else:
+            break
+
+    with open("assets\\users.json" , "r") as f:
+        x = json.load(f)
+
+    x[str(len(x)+1)] = {}
+    x[str(len(x))]["name"] = name
+    x[str(len(x))]["password"] = str(base64.b64encode(pwd.encode('ascii')))
+    x[str(len(x))]["user_id"] = str(len(x))
+
+    USER_ID = str(len(x))
+
+    with open("assets\\users.json" , "w") as f_2:
+        json.dump(x , f_2)
+
+    print("Succesfully Registered!\nYour USER ID is {}".format(USER_ID))
+
+    return 0
+
+def login() -> int:
+    uid = input("PLEASE ENTER YOUR UID (User ID): ")
+
+    with open("assets\\users.json" , "r") as f:
+        z = json.load(f)
+
+    if uid not in z:
+        print("Invalid User ID")
+        return 1
+    
+    else:
+        pwd = input("PLEASE ENTER YOUR PASSWORD: ")
+        if str(base64.b64encode(pwd.encode('ascii'))) != z[uid]["password"]:
+            print("Invalid Password")
+        else:
+            os.system('cls')
+            print("Welcome {}".format(z[uid]["name"]))
+
+        return 0
--- main.py
+++ main.py
@@ -1,3 +1,25 @@
 import app_tools as at
+import os
+
+os.system("@echo off")
+os.system("cls")
 
 print(at.INTRO_TEXT)
+
+def signin():
+    print('[R] - Register\n[L] - Login')
+    x = input("Welcome!\nPlease Choose if you want to register or login: ").lower()
+
+    return x
+
+signin_ = signin()
+
+if signin_ != 'r' and signin_ != 'l':
+    print("Thats an invalid choice please try again by running the application again")
+
+else:
+    if signin_ == 'r':
+        at.register()
+    else:
+        at.login()
+        print(at.MENU_UI)
